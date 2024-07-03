c6dc9142982ce52e704c56e7d93bda593065d072
thomasht86 <thomas@vespa.ai>
2024-07-03 10:19:49 +0200

    run testapp prod

---
+++ .github/workflows/deploy_to_prod.py
@@ -0,0 +1,37 @@
+# Command line script to deploy Vespa applications to Vespa Cloud
+# Usage: vespa-deploy.py --tenant --application --api-key --application-root --max-wait 3600 --source-url
+
+import argparse
+
+from vespa.deployment import VespaCloud
+
+
+def deploy_prod(
+    tenant, application, api_key, application_root, max_wait=3600, source_url=None
+):
+    vespa_cloud = VespaCloud(
+        tenant=tenant,
+        application=application,
+        key_content=api_key,
+    )
+    build_no = vespa_cloud.deploy_to_prod(application_root, source_url=source_url)
+    vespa_cloud.wait_for_prod_deployment(build_no, max_wait=max_wait)
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--tenant", required=True, help="Vespa Cloud tenant")
+    args.add_argument("--application", required=True, help="Vespa Cloud application")
+    args.add_argument("--api-key", required=True, help="Vespa Cloud API key")
+    args.add_argument(
+        "--application-root", required=True, help="Path to the Vespa application root"
+    )
+    args.add_argument(
+        "--max-wait", type=int, default=3600, help="Max wait time in seconds"
+    )
+    args.add_argument(
+        "--source-url", help="Source URL (git commit URL) for the deployment"
+    )
+
+    args = args.parse_args()
+    deploy_prod(args.tenant, args.application, args.api_key, args.application_root)
