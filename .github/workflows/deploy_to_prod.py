62773cd761ba8e36c922ef1fccd4e7af17ffd778
thomasht86 <thomas@vespa.ai>
2024-07-03 10:35:14 +0200

    set root

--- .github/workflows/deploy_to_prod.py
+++ .github/workflows/deploy_to_prod.py
@@ -13,6 +13,7 @@ def deploy_prod(
         tenant=tenant,
         application=application,
         key_content=api_key,
+        application_root=application_root,
     )
     build_no = vespa_cloud.deploy_to_prod(application_root, source_url=source_url)
     vespa_cloud.wait_for_prod_deployment(build_no, max_wait=max_wait)
